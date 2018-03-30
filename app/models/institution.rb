850b4895988d7921b9e01d3629693e463640da27
Julianna <julianna29kalinich@gmail.com>
2018-03-30 17:16:18 +0300

    tests for instution

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -4,12 +4,11 @@ class Institution < ApplicationRecord
    has_and_belongs_to_many :category
    has_one :work_time
    has_many :ratings
-   has_many :Institution_comments
+   has_many :institution_comments
 
    validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
    validates :description, length: { in: 10..300 }
    validates_format_of :phoneNumber, 
      lendth: { in: 12 },
       :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/
-
 end
--- spec/models/institution_spec.rb
+++ spec/models/institution_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Institution, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{
+		build(:institution)
+	}
+
+  context "validation" do
+ 		it "is valid with valid atributes" do
+ 			expect(subject).to be_valid
+		end
+	end
+
+	context "association" do
+  		it { should have_and_belong_to_many(:category)}
+  		it { should have_one(:work_time)}
+  		it { should have_many(:ratings)}
+  		it { should have_many(:institution_comments)}
+  	end
 end
